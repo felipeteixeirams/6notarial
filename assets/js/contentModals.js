a9417a79729c10d465152e0def734edefb397c66
Felipe Teixeira <ti@6notarial.com.br>
2020-04-08 08:38:40 -0400

    Alterações realizadas em casa

---
+++ assets/js/contentModals.js
@@ -0,0 +1,44 @@
+{
+    function conteudoAutenticacao() {
+        let el = document.getElementById('autenticacao');
+        let content = '<article><div class="modal-header"><h3>Autenticação de cópia</h3></div><div class="modal-body"><p>Texto exemplo.</p></div><div class="modal-footer"><button type="button" class="btn-primary" data-dismiss="modal">Fechar</button></div></article>';
+        el.innerHTML = content;
+    }
+
+    function conteudoReconhecimento() {
+        let el = document.getElementById('reconhecimento');
+        let content = '<article><div class="modal-header"><h3>Autenticação de cópia</h3></div><div class="modal-body"><p>Texto exemplo.</p></div><div class="modal-footer"><button type="button" class="btn-primary" data-dismiss="modal">Fechar</button></div></article>';
+        el.innerHTML = content;
+    }
+
+    function conteudoApostila() {
+        let el = document.getElementById('apostila');
+        let content = '<article><div class="modal-header"><h3>Autenticação de cópia</h3></div><div class="modal-body"><p>Texto exemplo.</p></div><div class="modal-footer"><button type="button" class="btn-primary" data-dismiss="modal">Fechar</button></div></article>';
+        el.innerHTML = content;
+    }
+
+    function conteudoEscritura() {
+        let el = document.getElementById('escritura');
+        let content = '<article><div class="modal-header"><h3>Autenticação de cópia</h3></div><div class="modal-body"><p>Texto exemplo.</p></div><div class="modal-footer"><button type="button" class="btn-primary" data-dismiss="modal">Fechar</button></div></article>';
+        el.innerHTML = content;
+    }
+
+    function conteudoProcuracao() {
+        let el = document.getElementById('procuracao');
+        let content = '<article><div class="modal-header"><h3>Autenticação de cópia</h3></div><div class="modal-body"><p>Texto exemplo.</p></div><div class="modal-footer"><button type="button" class="btn-primary" data-dismiss="modal">Fechar</button></div></article>';
+        el.innerHTML = content;
+    }
+
+    function conteudoTestamento() {
+        let el = document.getElementById('testamento');
+        let content = '<article><div class="modal-header"><h3>Autenticação de cópia</h3></div><div class="modal-body"><p>Texto exemplo.</p></div><div class="modal-footer"><button type="button" class="btn-primary" data-dismiss="modal">Fechar</button></div></article>';
+        el.innerHTML = content;
+    }
+
+    conteudoAutenticacao()
+    conteudoReconhecimento()
+    conteudoApostila()
+    conteudoEscritura()
+    conteudoProcuracao()
+    conteudoTestamento()
+}
