cfb34a5ece7e27636d4d7071c4bc85be55b153ca
Felipe Teixeira <felipeteixeirams@gmail.com>
2020-09-15 16:59:52 -0400

    Alteracoes do map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,8 @@
-(function (){
-    let el = document.getElementById("map")
-    let content = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3737.994506769025!2d-54.6160401!3d-20.4654188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9486e618a1483461%3A0xd41437c5f22b86ee!2zNsKwIFNlcnZpw6dvIE5vdGFyaWFs!5e0!3m2!1spt-BR!2sbr!4v1563823748125!5m2!1spt-BR!2sbr" width="100%" height="300" frameborder="0" style="border:0; box-sizing: border-box; margin: auto" allowfullscreen></iframe>`
-    el.innerHTML = content
+/* START MAP
+***********************************/
+(function(){
+    let el = document.getElementsById("map");
+    el.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3737.994506769025!2d-54.6160401!3d-20.4654188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9486e618a1483461%3A0xd41437c5f22b86ee!2zNsKwIFNlcnZpw6dvIE5vdGFyaWFs!5e0!3m2!1spt-BR!2sbr!4v1563823748125!5m2!1spt-BR!2sbr" height: 300px; width: 100%; style="border:0; box-sizing: border-box; margin: auto" allowfullscreen></iframe>`
 }())
+
+/* END MAP*/
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,73 +1,72 @@
-(function($) {
-    "use_strict";
+(function ($) {
+  "use_strict";
 
 
   // PRELOADER
-    $(window).on('load', function() {
-        $('#page-loader').fadeOut('slow', function() {
-            $(this).remove();
-        });
+  $(window).on('load', function () {
+    $('#page-loader').fadeOut('slow', function () {
+      $(this).remove();
     });
+  });
 
 
   // SCROLL TO TOP
 
   $(window).on('scroll', function () {
     if ($(window).scrollTop() > 70) {
-        $('.scroll-to-top').addClass('reveal');
+      $('.scroll-to-top').addClass('reveal');
     } else {
-        $('.scroll-to-top').removeClass('reveal');
+      $('.scroll-to-top').removeClass('reveal');
     }
-});
+  });
 
 
-/* ----------------------------------------------------------- */
-    /*  Fixed header
-    /* ----------------------------------------------------------- */
+  /* ----------------------------------------------------------- */
+  /*  Fixed header
+  /* ----------------------------------------------------------- */
 
 
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 70) {
-            $('.site-navigation,.trans-navigation').addClass('header-white');
-        } else {
-            $('.site-navigation,.trans-navigation').removeClass('header-white');
-        }
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 70) {
+      $('.site-navigation,.trans-navigation').addClass('header-white');
+    } else {
+      $('.site-navigation,.trans-navigation').removeClass('header-white');
+    }
 
-    });
+  });
 
 
 
   // Smooth scrolling using jQuery easing
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
 
 
-    $('a.js-scroll-trigger').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.js-scroll-trigger').on('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').on('click', function(event) {
+  $('.js-scroll-trigger').on('click', function (event) {
     $('.navbar-collapse').collapse('hide');
   });
 
 
 
-          /*START GOOGLE MAP*/
-          function initialize() {
-            var uluru = {lat: -25.344, lng: 131.036};
-            // The map, centered at Uluru
-            var map = new google.maps.Map(
-              document.getElementById('map'), {zoom: 4, center: uluru});
-            // The marker, positioned at Uluru
-            var marker = new google.maps.Marker({position: uluru, map: map});
-          }
-          google.maps.event.addDomListener(window, 'load', initialize);
-          /*END GOOGLE MAP*/
+  /*START GOOGLE MAP
+   ***************************************/
+  let map = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3737.994506769025!2d-54.6160401!3d-20.4654188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9486e618a1483461%3A0xd41437c5f22b86ee!2zNsKwIFNlcnZpw6dvIE5vdGFyaWFs!5e0!3m2!1spt-BR!2sbr!4v1563823748125!5m2!1spt-BR!2sbr" frameborder="0" allowfullscreen></iframe>`;
+
+  $(document).ready(function () {
+    $('#map').load(map);
+  });
+
+  
+  /*END GOOGLE MAP*/
 
 
 
