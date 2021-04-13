cf41df3473a52edde7c0cb0fe5b9487277ff1d46
Felipe Teixeira <felipeteixeirams@gmail.com>
2021-04-12 23:39:42 -0400

    Adicionado animação ao componente de tela de carregamento

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './LoadingScreen.css';
 
-loadingScreen = () => {
-   let el = document.getElementById("loading-screen");
-   el.className = "loading-screen-hidden";
-}
-
 const LoadingScreen = () =>{
-   
-
    return(
-      <div id="loading-screen"></div>
+      <div class="container">
+        <div class="square-to-circle"></div>
+      </div> 
    );
 }
 
 export default LoadingScreen;
+
+//https://betterprogramming.pub/a-quick-and-easy-react-js-loading-screen-with-hooks-940feccd553f
