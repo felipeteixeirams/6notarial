89eb6fd5b9190845f8bac00472f271fc9719ae64
Felipe Teixeira <felipeteixeirams@gmail.com>
2021-10-14 17:12:43 -0400

    Novo menu 'Privacidade'

---
+++ src/components/ScrollTop/ScrollTop.js
@@ -0,0 +1,8 @@
+import '@/index.css';
+import '@/_assents/css/responsive.css';
+
+export default function ScrollTop(){
+    return(
+        <button className="btn btn-primary scroll-to-top">hello</button>
+    );
+}
